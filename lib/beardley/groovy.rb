60cd08a2b4710df08e56d6ea261edabe13afd8eb
Brice TEXIER <brice@ekylibre.com>
2017-06-29 13:38:30 +0200

    Adds missing require file

--- lib/beardley/groovy.rb
+++ lib/beardley/groovy.rb
@@ -1,13 +1,11 @@
-require "beardley/groovy/version"
+require 'beardley/groovy/version'
 
 module Beardley
   module Groovy
-
     RjbLoader.before_load do |config|
-      Dir[Pathname.new(__FILE__).dirname.join("..", "..", "vendor", "java", "*.jar")].each do |path|
+      Dir[Pathname.new(__FILE__).dirname.join('..', '..', 'vendor', 'java', '*.jar')].each do |path|
         config.classpath << File::PATH_SEPARATOR + File.expand_path(path)
       end
     end
-
   end
 end
--- lib/beardley/groovy/version.rb
+++ lib/beardley/groovy/version.rb
@@ -1,5 +1,5 @@
 module Beardley
   module Groovy
-    VERSION = "2.0.0"
+    VERSION = '2.0.1'.freeze
   end
 end
